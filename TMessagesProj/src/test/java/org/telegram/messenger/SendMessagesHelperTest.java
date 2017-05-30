5c7461ad7e2bd26fe30116e3bc4ddb1abce96e60
Menzies <stefan_schenk@hotmail.com>
2017-05-30 11:41:38 +0200

    JUnit test

--- TMessagesProj/src/test/java/org/telegram/messenger/SendMessagesHelperTest.java
+++ TMessagesProj/src/test/java/org/telegram/messenger/SendMessagesHelperTest.java
@@ -1,31 +1,46 @@
 package org.telegram.messenger;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.telegram.messenger.SendMessagesHelper;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by stefa on 23-May-17.
  */
 public class SendMessagesHelperTest {
+
+    SendMessagesHelper sendMessagesHelper;
+    ArrayList<MessageObject> messages;
+
+    @Before
+    public void setUp() {
+        this.sendMessagesHelper = new SendMessagesHelper();
+    }
+
     @Test
-    public void sendMessage() throws Exception {
-        assertTrue(true);
+    public void testSendMessage() throws Exception {
+        this.messages = new ArrayList<>();
+        MessageObject message = mock(MessageObject.class);
+        this.messages.add(message);
+
+        sendMessagesHelper.sendMessage(messages, 1);
+
+        assertTrue(this.messages.size() == 0);
     }
 
     @Test
     public void editMessage() throws Exception {
-        assertTrue(true);
-    }
 
-    @Test
-    public void sendMessage1() throws Exception {
-        assertTrue(true);
     }
 
     @Test
     public void isSendingMessage() throws Exception {
-        assertTrue(true);
+
     }
 
 }
